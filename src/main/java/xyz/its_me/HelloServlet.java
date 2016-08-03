9f90e68a9d0e1b179dd9417a0e980f9f9af27989
Torsten Werner <torsten.werner@westernacher.com>
2016-08-03 08:25:45 +0200

    add message.properties to show that resource access is working

--- src/main/java/xyz/its_me/HelloServlet.java
+++ src/main/java/xyz/its_me/HelloServlet.java
@@ -7,13 +7,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @WebServlet(name = "MyServlet", urlPatterns = {"/hello"})
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (final ServletOutputStream out = resp.getOutputStream()) {
-            out.write("Hello from your beautiful Servlet!".getBytes());
+            final InputStream messageStream = getClass().getResourceAsStream("message.properties");
+
+            final Properties messages = new Properties();
+            messages.load(messageStream);
+
+            final String welcome = messages.getProperty("welcome");
+
+            out.write(welcome.getBytes());
             out.flush();
         }
     }
